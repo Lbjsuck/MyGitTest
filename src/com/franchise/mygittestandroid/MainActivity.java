d466607bfd2c24095940d06bdb5932c648082857
franc <franc@franc-PC>
2016-04-21 16:27:22 +0800

    在公司修改代码

--- src/com/franchise/mygittestandroid/MainActivity.java
+++ src/com/franchise/mygittestandroid/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//for git test
+		//edit in company
 		setContentView(R.layout.activity_main);
 	}
 
